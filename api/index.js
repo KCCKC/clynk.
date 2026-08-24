eccb1d06b2198f30b7819aa8a18c400de671cf9d
KCCKC <karchungchong@gmail.com>
2026-08-24 12:05:05 +0800

    Delete api directory

--- api/index.js
+++
@@ -1,5 +0,0 @@
-export default function handler(q,r){
-r.setHeader('Access-Control-Allow-Origin','*');
-r.setHeader('Content-Type','application/json');
-r.end(JSON.stringify({success:true,pin:'V0',value:'24.8',title:'OK',insights:'Nominal',actions:[],data:[]}));
-}
